eb40af7b71ee0741c4f72cb7352ea534e47dae99
tanuj bhandari <tanujbhandari10@gmail.com>
2025-02-09 18:07:51 +0000

    datatype and variables

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+// alert(4 + 4)  we are using nodejs, not browser
+console.log(3 
+    +
+    3
+) // code readability should be high
+console.log("tanuj")
+
+let name = "tanuj" //string
+let age = 23
+let isLoggedIn = false
+//number => 2 to power 53
+// bigint 
+// string =>""
+//boolean => true/false
+//null => standalone value
+//undefined => not defined
+// symbol => unique
+
+
+// object 
+console.log(typeof undefined)
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountid = 1444
+let accountemail = "hiteshsir"
+var accountPass = "jaipur"
+accountcity ="delhi" //possible
+//accountid = 2   not allowed
+let accountstate;
+console.log(accountid)
+accountemail= "tanuj@gmail.com"
+console.table([accountid, accountemail,accountPass,accountcity,accountstate])
+/* 
+    prefer not to use var
+    because of issue in block scope and fuctional scope
+*/
