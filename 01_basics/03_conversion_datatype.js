7630ccd08bca0389d8717e5ffb1acbc774e8d63a
tanuj bhandari <tanujbhandari10@gmail.com>
2025-02-09 18:26:30 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion_datatype.js
@@ -0,0 +1,28 @@
+let score =true
+console.log(typeof(score))
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/*
+ "33" => 33
+ "33abc" => NAN
+ true => 1
+ false => 0
+ */
+let isLoggedIn = "true"
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+
+// 1 => true
+// 0 => false
+// "" => false
+// "tanuj" => true 
+
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
