d8a95444cf17aa7e3ff97bb513956c8a33258224
tanuj bhandari <tanujbhandari10@gmail.com>
2025-02-10 13:53:32 +0000

    datatype_summerty

---
+++ 01_basics/datatypes_summery.js
@@ -0,0 +1,48 @@
+// primitive data types
+// 7
+// String ,number, Boolean, null, undefined, Symbol
+// BigInt
+
+// Reference (Non primitive)
+// Array , Objects, Function
+
+// dynamically typed 
+
+// let userEmail;
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id === anotherid)
+const bigNumber = 34567890123n;
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "hitesh",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("Aditi")
+}
+
+console.log(typeof bigNumber);
+
+Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
+
+
+
+
